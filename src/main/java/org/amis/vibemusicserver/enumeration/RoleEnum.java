e66624b3dc9e29c5ac10ed8c7eb9df60e9fa720a
AmisKwok <zicongkwok@gmail.com>
2026-01-04 05:16:10 +0800

    [更新]添加了注释

--- src/main/java/org/amis/vibemusicserver/enumeration/RoleEnum.java
+++ src/main/java/org/amis/vibemusicserver/enumeration/RoleEnum.java
@@ -4,7 +4,9 @@ import lombok.Getter;
 
 /**
  * 用户角色枚举类
+ * <p>
  * 定义系统中可用的用户角色类型
+ * </p>
  *
  * @author KwokChichung
  */
@@ -12,13 +14,17 @@ import lombok.Getter;
 public enum RoleEnum {
     /**
      * 管理员角色
+     * <p>
      * 拥有系统的最高权限，可以执行所有操作
+     * </p>
      */
     ADMIN("ROLE_ADMIN"),
 
     /**
      * 普通用户角色
+     * <p>
      * 拥有基本的系统操作权限
+     * </p>
      */
     USER("ROLE_USER");
 
@@ -26,6 +32,7 @@ public enum RoleEnum {
 
     /**
      * 枚举构造函数
+     *
      * @param role 角色标识字符串
      */
     RoleEnum(String role) {
